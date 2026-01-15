57bf8e4e7cdb72617abadad06c3e7c6728bda093
Bohao Chen <165555025+wisx1@users.noreply.github.com>
2026-01-14 17:24:23 -0800

    update run.py

--- winter/run.py
+++ winter/run.py
@@ -1,8 +1,14 @@
-from generate import *
-from verify import *
+from dotenv import load_dotenv
+from generate import generate
+from generate_concurrent import generate_concurrent
+from verify_2 import verify
+from verify_parallel import verify_parallel
 
 load_dotenv("../.env")
 
-#generate("../data/winter/test3B.jsonl", "../data/winter/test3C.jsonl", "us.anthropic.claude-sonnet-4-5-20250929-v1:0", 0, True)
-#verify("../data/winter/test3C.jsonl", "../data/winter/test3C.jsonl")
-print(check_accuracy("../data/winter/test3C.jsonl"))
+if __name__ == "__main__":
+    # generate("../data/1.jsonl", "../data/2.jsonl", "us.anthropic.claude-sonnet-4-5-20250929-v1:0", 0, False)
+    generate_concurrent("../data/1.jsonl", "../data/3.jsonl", "us.anthropic.claude-sonnet-4-5-20250929-v1:0", 0, False, 4)
+    # verify("../data/test3_cleaned.jsonl", "../data/test1.jsonl")
+    # verify_parallel("../data/test3_cleaned.jsonl", "../data/test.jsonl", 4)
+    # print(check_accuracy("../data/test.jsonl"))
