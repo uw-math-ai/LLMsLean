437ab50b67a69165c20c53a63f86082dadecc919
Escher Crawford <efcawesome@gmail.com>
2026-01-18 22:54:35 -0800

    for real this time fix kimina_test

--- winter/kimina_test.py
+++ winter/kimina_test.py
@@ -1,8 +1,8 @@
 from langchain_huggingface import HuggingFacePipeline
 from transformers import AutoModelForCausalLM, AutoTokenizer, pipeline
-import os
+from dotenv import load_dotenv
 
-os.environ["HF_HOME"] = "/gpfs/projects/mathai/lean-bench/LLMsLean/models"
+load_dotenv("../.env")
 
 model_id = "AI-MO/Kimina-Prover-72B"
 tokenizer = AutoTokenizer.from_pretrained(model_id)
