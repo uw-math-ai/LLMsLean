5532526bde5f02c01fc915bcc5306d2bf07e6ad0
Escher Crawford <efcawesome@gmail.com>
2026-01-18 21:42:46 -0800

    fix init_model for huggingface models

--- winter/init_model.py
+++ winter/init_model.py
@@ -1,25 +1,32 @@
 from langchain.chat_models import init_chat_model, BaseChatModel
 from langchain_huggingface import HuggingFacePipeline
-from transformers import AutoModelForCausalLM, AutoTokenizer, pipeline
 
 _MODELS = {
   "sonnet": "us.anthropic.claude-sonnet-4-5-20250929-v1:0",
-  "kimina": "../models/Kimina-Prover-Preview-Distill-7B"
+  "kimina": "AI-MO/Kimina-Prover-Preview-Distill-7B"
 }
 
 _LOCAL_MODELS = {"kimina", "deepseek", "goedel"}
 
 def init_model(model_name: str, temp: float) -> BaseChatModel:
-    model_path = _MODELS[model_name]
+    model_id = _MODELS[model_name]
 
     if model_name in _LOCAL_MODELS:  # local models
-        tokenizer = AutoTokenizer.from_pretrained(model_path)
-        model = AutoModelForCausalLM.from_pretrained(model_path)
-        pipe = pipeline("text-generation", model=model, tokenizer=tokenizer, device_map="auto")
-        llm = HuggingFacePipeline(pipeline=pipe)
+        llm = HuggingFacePipeline.from_model_id(
+            model_id=model_id,
+            task="text-generation",
+            pipeline_kwargs={
+                "do_sample": True,
+                "temperature": temp,
+            },
+            device_map="auto", 
+        )
+        
+        resp = llm.invoke("Prove that 2+2=4")
+        print(resp)
     elif model_name == "sonnet":  # anthropic models
-        llm = init_chat_model(model_path, temperature=temp)
+        llm = init_chat_model(model_id, temperature=temp)
     else:  # bedrock models
-        llm = init_chat_model(model_path, temperature=temp, model_provider="bedrock_converse")
+        llm = init_chat_model(model_id, temperature=temp, model_provider="bedrock_converse")
 
     return llm
